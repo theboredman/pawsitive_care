9cfca36aeac54a7f6cad7ecf3e4d8a994367b63e
Asadullah Hil Galib <imgalibb@gmail.com>
2025-08-21 02:01:43 +0600

    Dashboards updated

--- pawsitive_care/staticfiles/admin/js/collapse.js
+++ pawsitive_care/staticfiles/admin/js/collapse.js
@@ -1,7 +1,10 @@
+collaps.js
+
+
 /*global gettext*/
 'use strict';
 {
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
         // Add anchor tag for Show/Hide link
         const fieldsets = document.querySelectorAll('fieldset.collapse');
         for (const [i, elem] of fieldsets.entries()) {
@@ -20,7 +23,7 @@
             }
         }
         // Add toggle to hide/show anchor tag
-        const toggleFunc = function(ev) {
+        const toggleFunc = function (ev) {
             if (ev.target.matches('.collapse-toggle')) {
                 ev.preventDefault();
                 ev.stopPropagation();
@@ -36,7 +39,7 @@
                 }
             }
         };
-        document.querySelectorAll('fieldset.module').forEach(function(el) {
+        document.querySelectorAll('fieldset.module').forEach(function (el) {
             el.addEventListener('click', toggleFunc);
         });
     });
